4358570a17310881991e1725b752acd50699bc6e
Ziyue Jin <ziyuejin877@gmail.com>
2024-11-21 21:49:03 -0800

    init

---
+++ appointment-management-app/src/main/java/org/dljl/AppointmentManagementApp.java
@@ -0,0 +1,12 @@
+package org.dljl; // Replace with your base package
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentManagementApp {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AppointmentManagementApp.class, args);
+  }
+}
---
+++ appointment-management-app/src/main/java/org/dljl/repository/UserInfoRepository.java
@@ -0,0 +1,12 @@
+package org.dljl.repository;
+
+import org.dljl.entity.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
+  Optional<UserInfo> findByEmail(String email); // Use 'email' if that is the correct field for login
+}
